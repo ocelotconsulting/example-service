52bf37f0db712fb2bb592eca772b2bb1001991e4
John O'Malley <john.omalley@ocelotconsulting.com>
2016-10-26 10:30:20 -0500

    checkout

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,6 +5,7 @@ curl --silent --location https://rpm.nodesource.com/setup_6.x | bash -
 yum -y install nodejs
 git clone https://github.com/ocelotconsulting/example-service.git
 cd example-service
+git checkout v2
 npm install -g pm2
 npm install
 pm2 start server.js
