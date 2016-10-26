dcf920278bf1c30ad618e53bb267ea862843c677
John O'Malley <john.omalley@ocelotconsulting.com>
2016-10-26 10:12:12 -0500

    final script

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
-sudo yum update -y
-sudo yum install git -y
+yum -y update
+yum -y install git
+curl --silent --location https://rpm.nodesource.com/setup_6.x | bash -
+yum -y install nodejs
 git clone https://github.com/ocelotconsulting/example-service.git
 cd example-service
-sudo curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.32.1/install.sh | bash
-export NVM_DIR="/home/ec2-user/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
-nvm install 6.9.1
 npm install -g pm2
 npm install
 pm2 start server.js
